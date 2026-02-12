8f7d58b1ff6e55031fe565e06bd41721a83e8846
Alex Foster <wookie471@outlook.com>
2026-02-12 16:04:39 +0000

    progress bar added spam prevention added

--- compression.py
+++ compression.py
@@ -1,10 +1,16 @@
 import os, ffmpeg
 import tkinter as tk
 from tkinter import filedialog
+from threading import Thread
+from tkinter.ttk import *
+import math
 
 global compMode
 global filePath
 global folderPath
+global t
+global counter
+global prog
 compMode = 0
 filePath = ""
 folderPath = ""
@@ -37,9 +43,11 @@ def compress_video(video_full_path, output_file_name, target_size):
     ffmpeg.output(i, os.devnull,
                   **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
                   ).overwrite_output().run()
+    progress['value'] += prog
     ffmpeg.output(i, output_file_name,
                   **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
                   ).overwrite_output().run()
+    progress['value'] += prog
 
 
 
@@ -70,9 +78,11 @@ def folderSelect():
 def compression():
     global filePath
     global folderPath
-    print("function clicked")
-    print(compMode)
+    global prog
     val = entry.get()
+
+    tempCount = 0
+    prog = 0
     if val.isnumeric():
         val = int(val)
     else:
@@ -80,17 +90,53 @@ def compression():
     if compMode == 1:
         for file in os.scandir(folderPath):
             if ".mp4" in file.name:
+                tempCount += 1
+        
+        progress['value'] = 0
+        prog = 100/tempCount/2
+        prog = math.floor(prog)
+        for file in os.scandir(folderPath):
+            if ".mp4" in file.name:
                 out = file.path
                 out = out[:-4]
                 compress_video(file.path, out +"_Compressed_Ver.mp4", val * 1000)
+                
 
     if compMode == 0:
+        progress['value'] = 0
+        prog = 50
         out = filePath
         out = out[:-4]
         compress_video(filePath, out +"_Compressed_Ver.mp4", val * 1000)
+    
+    progress['value'] = 100
+
+def compThread():
+    global t
+    global counter
+    compressButton['state'] = 'disable'
+    fileSelect['state'] = 'disable'
+    folderSelect['state'] = 'disable'
+    counter = 0
+    t = Thread(target=compression, daemon=True)
+    t.start()
+    check_thread()
         
 
+def check_thread():
+    global counter
 
+    if not t.is_alive():
+        compressButton['state'] = 'normal'
+        fileSelect['state'] = 'normal'
+        folderSelect['state'] = 'normal'
+        label['text'] = ''
+    else:
+        label['text'] = str(counter)
+        counter += 0.1
+
+        # check again after 100ms
+        root.after(100, check_thread) 
 
 root = tk.Tk()
 root.title("Compressor")
@@ -104,8 +150,11 @@ fileSelect.place(x=20,y=20)
 folderSelect = tk.Button(root, text="Select for whole folder compression", command=folderSelect)
 folderSelect.place(x=20,y=120)
 #compress button
-folderSelect = tk.Button(root, text="Click to compress selected file(s)", command=compression)
-folderSelect.place(x=20,y=220)
+compressButton = tk.Button(root, text="Click to compress selected file(s)", command=compThread)
+compressButton.place(x=20,y=220)
+#progress bar
+progress = Progressbar(root, length=200, mode='determinate')
+progress.place(x=20,y=250)
 #compress size entry
 label = tk.Label(root, text="Enter the size you want your videos compressed to (MB)")
 entry = tk.Entry(root)
